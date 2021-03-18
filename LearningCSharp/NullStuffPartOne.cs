68f9a27358526089ad20e186e55392bd8a8d5664
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-18 16:44:28 -0400

    Done with Thursday

---
+++ LearningCSharp/NullStuffPartOne.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class NullStuffPartOne
+    {
+
+        [Fact]
+        public void ConditionalAssignment()
+        {
+            var age = 20;
+            string message = age >= 21 ? "Old Enough" : "Too Young";
+            //if(age>= 21)
+            //{
+            //    message = "Old Enough";
+            //} else
+            //{
+            //    message = "Too Young";
+            //}
+            
+            Assert.Equal("Too Young", message);
+        }
+        [Fact]
+        public void NullCoalescingOperator()
+        {
+            string firstName = null, lastName = null;
+
+            string preferredName = firstName ?? lastName ?? "Unknown";
+            //if(firstName == null)
+            //{
+            //    preferredName = lastName;
+            //} else
+            //{
+            //    preferredName = firstName;
+            //}
+
+            Assert.Equal("Unknown", preferredName);
+        }
+        [Fact]
+        public void ElvisOperator()
+        {
+            Dinner d = new Dinner { MainCourse = "Tacos" };
+
+            var mainCourse = d?.MainCourse ?? "None Assigned";
+            //if(d != null)
+            //{
+            //    mainCourse = d.MainCourse;
+            //} else
+            //{
+            //    mainCourse = "Non Assigned";
+            //}
+
+            Assert.Equal("None Assigned", mainCourse);
+        }
+    }
+}
